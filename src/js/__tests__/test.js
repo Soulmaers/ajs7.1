18cb12ca22fa997ed95d0c42f5abb10f17161ea2
Soulmaers <certiss@ya.ru>
2022-06-22 16:46:22 +0300

    готово

--- src/js/__tests__/test.js
+++ src/js/__tests__/test.js
@@ -1,9 +1,46 @@
+import Team from '../app';
 
-import Team from '../app'
+test('проверка добавления', () => {
+  const person = new Team();
+  const character = {
 
-test('тест метода add', () => {
-    const person = new Team();
-    person.members.add('character')
-    expect(person.members.has('character')).toEqual('character');
+  };
+  person.add(character);
 
-})
+  expect(person.members.has(character)).toBe(true);
+});
+
+test('проверка ошибки дубля', () => {
+  const person = new Team();
+  const character = {
+  };
+  person.add(character);
+
+  function twoAdd() {
+    person.add(character);
+  }
+
+  expect(twoAdd).toThrowError('Уже есть');
+});
+
+test('добавление уникальных объектов', () => {
+  const person = new Team();
+
+  const bowman = {};
+  const magic = {};
+  person.addAll(magic, bowman, magic, bowman);
+
+  const res = new Set([magic, bowman]);
+  expect(person.members).toEqual(res);
+});
+
+test('конвертация в массив', () => {
+  const person = new Team();
+
+  const bowman = {};
+  const magic = {};
+  person.addAll(magic, bowman, magic, bowman);
+
+  const res = [magic, bowman];
+  expect(person.toArray()).toEqual(res);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,26 @@
-
-
-
 export default class Team {
-    constructor() {
-        this.members = new Set();
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(name) {
+    if (this.members.has(name)) {
+      throw new Error('Уже есть');
+    } else {
+      this.members.add(name);
     }
-    add(name) {
-        this.members.forEach((el) => {
-            if (el !== name) {
-                this.members.add(name);
-            }
-            else {
-                throw new Error('Дубль')
-            }
-        })
+  }
 
-
+  addAll(...names) {
+    for (let i = 0; i < names.length; i += 1) {
+      if (!this.members.has(names[i])) { this.members.add(names[i]); }
     }
-    addAll() {
+  }
 
-    }
-    toArray() {
+  toArray() {
+    const arrPerson = [];
+    this.members.forEach((el) => arrPerson.push(el));
 
-
-    }
+    return arrPerson;
+  }
 }
